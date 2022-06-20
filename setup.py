c23e7e70192cac3f655b34dcf4aee5d7d60c249c
Brennan Abanades Kenyon <56822523+brennanaba@users.noreply.github.com>
2022-06-20 12:10:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
     name='kasearch',
-    version='0.0.1',
+    version='0.0.2',
     description='Search OAS for similar sequences',
     license='BSD 3-clause license',
     maintainer='Tobias Olsen; Brennan Abanades',
@@ -16,6 +16,8 @@ setup(
         '': ['*.txt']
     },
     install_requires=[
+        'pandas',
+        'joblib',
         'numpy',
         'numba',
     ],
