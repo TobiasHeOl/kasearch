766594c0fa169af0f478469391d3d62dc3f4e94c
Tobias Olsen <olsen@iraqbabbler.stats.ox.ac.uk>
2022-08-02 00:18:34 +0100

    update multi query

--- kasearch/kasearch.py
+++ kasearch/kasearch.py
@@ -19,6 +19,29 @@ class SearchDB(ExtractMetadata):
                  id_to_study_file=None,
                 ):
         super().__init__()
+        
+        self.n_jobs = n_jobs
+        
+        self.__set_database_path(database_path)
+        self._set_id_to_study(os.path.join(self.database_path, "id_to_study.txt"))
+        self.__set_files_to_search(allowed_chain, allowed_species)
+        self.__reset_current_best()
+    
+    def __reset_current_best(self, qsize=1):
+        """
+        Resets currently most similar sequences.
+        """
+        
+        self._current_target_numbering = None
+        self._current_target_ids = None
+        self.current_best_identities = np.zeros((qsize, 1, 3), np.float16) - 1
+        self.current_best_ids = np.zeros((qsize, 1, 3, 2), np.int32) - 1
+
+    def __set_database_path(self, database_path = 'oasdb-small'):
+        """
+        Sets database path. If none given, downloads a small version of OAS (4.4GB).
+        """
+        
         if database_path == 'oasdb-small': # Download a small version of OAS
             db_folder = os.path.join(os.path.dirname(__file__), "oasdb")
             os.makedirs(db_folder, exist_ok = True)
@@ -37,39 +60,33 @@ class SearchDB(ExtractMetadata):
             database_path = glob.glob(os.path.join(db_folder, "oasdb_small*"))[0]
         
         self.database_path = database_path
-        self.n_jobs = n_jobs
         
-        self._set_id_to_study(os.path.join(database_path, "id_to_study.txt"))
-        self.__set_allowed_files(allowed_chain, allowed_species)
-        self.__reset_current_best()
-    
-    def __reset_current_best(self, qsize=1):
+    def __set_files_to_search(self, allowed_chain, allowed_species):
+        """
+        Sets files to search.
+        """
         
-        self._current_target_numbering = None
-        self._current_target_ids = None
-        self.current_best_identities = np.zeros((qsize, 1, 3), np.float16) - 1
-        self.current_best_ids = np.zeros((qsize, 1, 3, 2), np.int32) - 1
-
-    def __set_allowed_files(self, allowed_chain, allowed_species):
-        
-        self.allowed_normal_files = []
-        self.allowed_unusual_files = []
+        self.files_to_search_normal = []
+        self.files_to_search_unusual = []
         
         if allowed_species == 'Any': allowed_species = '*'
         allowed_species = [allowed_species] if isinstance(allowed_species, str) else allowed_species
         if allowed_chain == 'Any': allowed_chain = '*'
         
         for species in allowed_species:
-            self.allowed_normal_files += glob.glob(os.path.join(self.database_path, 
+            self.files_to_search_normal += glob.glob(os.path.join(self.database_path, 
                                                          allowed_chain, 
                                                          species, 
                                                          "*data-subset-normal-*.npz"))
-            self.allowed_unusual_files += glob.glob(os.path.join(self.database_path, 
+            self.files_to_search_unusual += glob.glob(os.path.join(self.database_path, 
                                                                allowed_chain, 
                                                                species, 
                                                                "*data-subset-unusual-*.npz"))
 
     def __update_best(self, query, keep_best_n):
+        """
+        Update the current most similar sequences.
+        """
         
         chunk_best_identities, chunk_best_ids = get_n_most_identical_multiquery(query,
                                                                      self._current_target_numbering,
@@ -86,15 +103,18 @@ class SearchDB(ExtractMetadata):
         self.current_best_ids = np.take_along_axis(all_ids, order[:, :, :, None], axis=1)[:, :keep_best_n]
         
     def search(self, query, keep_best_n=10, reset_best=True):
+        """
+        Search database for sequences most similar to the queries.
+        """
         
         if reset_best == True:
             self.__reset_current_best(query.shape[0])
 
-        data_loader = DataLoader(self.allowed_normal_files[0])
+        data_loader = DataLoader(self.files_to_search_normal[0])
         self._current_target_numbering = data_loader.data['numberings']
         self._current_target_ids = data_loader.data['idxs']
         
-        for file in self.allowed_normal_files[1:]:
+        for file in self.files_to_search_normal[1:]:
             data_loader = DataLoader(file)
             self.__update_best(query, keep_best_n)
             self._current_target_numbering = data_loader.data['numberings']
@@ -103,9 +123,12 @@ class SearchDB(ExtractMetadata):
         self.__update_best(query, keep_best_n)
         
     def get_meta(self, n_query = 0, n_region = 0, n_sequences = 'all', n_jobs=1):
+        """
+        Get meta data for the current most similar sequences for a specific query and region.
+        """
         
         if n_sequences == 'all':
-            n_sequences = len(self.current_best_ids)
+            n_sequences = len(self.current_best_ids[1])
             
         assert n_query >= 0
         assert n_region >= 0
--- kasearch/prepare_query.py
+++ kasearch/prepare_query.py
@@ -8,12 +8,12 @@ from kasearch.canonical_alignment import canonical_alignment, canonical_numberin
 
 class AlignSequences:
     """
-    Prepares queries for KA-Search. 
+    Align sequences for KA-Search. 
     """
     def __init__(self, seqs, allowed_species=['Human', 'Mouse'], n_jobs=1):
         
         self.n_jobs = n_jobs
-        self._abnormal_sequence = np.zeros(canonical_numbering_len, np.int8)
+        self._unusual_sequence = np.zeros(canonical_numbering_len, np.int8)
         
         if allowed_species:
             self.allowed_species = [i.lower() for i in allowed_species]
@@ -23,14 +23,20 @@ class AlignSequences:
         self.db = self._many_canonical_alignment(seqs)
         
     def _canonical_alignment(self, seq):
+        """
+        Alignment of a single sequence.
+        """
         
         try:
             numbered_sequence, _ = number(seq, allowed_species=self.allowed_species)
             return canonical_alignment(numbered_sequence)
         except Exception:
-            return self._abnormal_sequence
+            return self._unusual_sequence
     
     def _many_canonical_alignment(self, seqs):
+        """
+        Alignment of multiple sequences.
+        """
         
         n_jobs = len(seqs) if len(seqs) <  self.n_jobs else self.n_jobs
         chunksize=len(seqs) // n_jobs
@@ -41,5 +47,7 @@ class AlignSequences:
         
 @dataclass   
 class Sequences:
-    
+    """
+    Sequence data class.
+    """
     aligned_seqs: None
