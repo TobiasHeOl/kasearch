ea3004c9831a2e71cc784088520f3341463b7a15
Tobias Olsen <olsen@iraqbabbler.stats.ox.ac.uk>
2022-08-22 15:13:33 +0100

    updated examples

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
     name='kasearch',
-    version='0.0.4',
+    version='0.0.6',
     description='KA-Search: Rapid and exhaustive sequence identity search of known antibodies',
     license='BSD 3-clause license',
     maintainer='Tobias Hegelund Olsen; Brennan Abanades kenyon',
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'pandas',
         'joblib',
-        'requests'
+        'requests',
         'numpy',
         'jax',
         'jaxlib'
